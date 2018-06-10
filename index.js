6fdbf4e3cd29b1611b6ba454deac72a21e7158a4
Lara Sanders <larasanders@gmail.com>
2018-06-10 16:44:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ console.log(driver.name)
 
 const logDriversByHometown = function(drivers, locations) {
 drivers.forEach(function (driver, location) {
-if(hometown === location)
+  driver.hometown === location
 console.log(driver.name)
 });
 }
