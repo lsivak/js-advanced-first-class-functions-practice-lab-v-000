5419fb5f9b7f94cec0753d7d0a617c800be3ab57
Lara Sanders <larasanders@gmail.com>
2018-06-10 17:18:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ console.log(driver.name)
 });
 }
 const driversByRevenue = function(drivers) {
-   drivers.sort(function(a, b) {
+  return drivers.sort(function(a, b) {
     return a.revenue - b.revenue
 })
 debugger
